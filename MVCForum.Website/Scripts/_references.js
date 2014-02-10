8bd733a6865973f0bbcaea219b71c27831add0a4
kangjh0815 <kangjh0815@live.com>
2014-02-09 20:20:24 -0800

    test

---
+++ MVCForum.Website/Scripts/_references.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="~/Scripts/jquery-1.8.0.min.js" />
+/// <reference path="~/Scripts/jquery-2.0.1.js" />
+/// <reference path="~/Scripts/jquery-2.8.1.min.js" />
+
+
+/// <reference path="jquery-ui-1.8.11.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="knockout-2.0.0.debug.js" />
+/// <reference path="modernizr-2.0.6-development-only.js" />
